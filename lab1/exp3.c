627ea12737b086abf4dfec0303c51dc08dd17f22
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2023-08-23 11:55:42 +0530

    made corrections

--- lab1/exp3.c
+++ lab1/exp3.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
-struct employee{
+
+struct employee {
     char Empid[20];
     char Name[20];
     char Designation[30];
     float basic_salary;
     float hra;
     float da;
-}emp [100];
+} emp[100];
 
-void main()
-{
-    int n,i;
-    printf("enter the number of employees:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        printf("enter the name of the employee: ");
-        scanf("%s",&emp[i].Name);
-        printf("enter the desig of the employee: ");
-        scanf("%s",&emp[i].Designation);
-        printf("enter the empid of the employee: ");
-        scanf("%s",&emp[i].Empid);
-        printf("enter the basic salary of the employee: ");
-        scanf("%f",&emp[i].basic_salary);
-         emp[i].hra = emp[i].basic_salary*(45/100);
-        emp[i].da = emp[i].basic_salary*(15/100);
-        }
-    printf("the details of employees are:");
-    for(i=0;i<n;i++)
-    {
-        float grs;
-        printf("name:         %s\n",emp[i].Name);
-        printf("designation:  %s\n",emp[i].Designation);
-        printf("empid:        %s\n",emp[i].Empid);
-        printf("salary:       %f\n",emp[i].basic_salary);
-        printf("hra:          %f\n",emp[i].hra);
-        printf("da:           %f\n",emp[i].da);
-        grs=emp[i].basic_salary+(((emp[i].hra)/100)*emp[i].basic_salary)+(((emp[i].da)/100)*emp[i].basic_salary);
-        printf("gross salary: %f\n",grs);
-        
-
+int main() {  // Use int instead of void for main
+    int n, i;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++) {
+        printf("Enter the name of the employee: ");
+        scanf("%s", emp[i].Name);  // Removed the '&' before emp[i].Name
+        printf("Enter the designation of the employee: ");
+        scanf("%s", emp[i].Designation);  // Removed the '&' before emp[i].Designation
+        printf("Enter the empid of the employee: ");
+        scanf("%s", emp[i].Empid);  // Removed the '&' before emp[i].Empid
+        printf("Enter the basic salary of the employee: ");
+        scanf("%f", &emp[i].basic_salary);
+        emp[i].hra = emp[i].basic_salary * (45.0 / 100);  // Added .0 for correct division
+        emp[i].da = emp[i].basic_salary * (15.0 / 100);   // Added .0 for correct division
     }
-
-
+    
+    printf("The details of employees are:\n");
+    for (i = 0; i < n; i++) {
+        float grs;
+        printf("Name:         %s\n", emp[i].Name);
+        printf("Designation:  %s\n", emp[i].Designation);
+        printf("Empid:        %s\n", emp[i].Empid);
+        printf("Salary:       %f\n", emp[i].basic_salary);
+        printf("HRA:          %f\n", emp[i].hra);
+        printf("DA:           %f\n", emp[i].da);
+        grs = emp[i].basic_salary + emp[i].da + emp[i].hra;
+        printf("Gross Salary: %f\n", grs);
+    }
+    
+    return 0;  // Return 0 to indicate successful execution
 }
