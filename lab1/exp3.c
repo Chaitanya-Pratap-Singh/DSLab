cea8ff43f58fd60a14d3faa79d20b201585b3b8a
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2023-08-23 12:12:08 +0530

    new changes

--- lab1/exp3.c
+++ lab1/exp3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-struct employee {
+struct employee
+{
     char Empid[20];
     char Name[20];
     char Designation[30];
@@ -9,26 +10,34 @@ struct employee {
     float da;
 } emp[100];
 
-int main() {  // Use int instead of void for main
+int main()
+{ // Use int instead of void for main
     int n, i;
+    float hrap, dap;
     printf("Enter the number of employees: ");
     scanf("%d", &n);
-    
-    for (i = 0; i < n; i++) {
+
+    for (i = 0; i < n; i++)
+    {
         printf("Enter the name of the employee: ");
-        scanf("%s", emp[i].Name);  // Removed the '&' before emp[i].Name
+        scanf("%s", emp[i].Name); // Removed the '&' before emp[i].Name
         printf("Enter the designation of the employee: ");
-        scanf("%s", emp[i].Designation);  // Removed the '&' before emp[i].Designation
+        scanf("%s", emp[i].Designation); // Removed the '&' before emp[i].Designation
         printf("Enter the empid of the employee: ");
-        scanf("%s", emp[i].Empid);  // Removed the '&' before emp[i].Empid
+        scanf("%s", emp[i].Empid); // Removed the '&' before emp[i].Empid
         printf("Enter the basic salary of the employee: ");
         scanf("%f", &emp[i].basic_salary);
-        emp[i].hra = emp[i].basic_salary * (45.0 / 100);  // Added .0 for correct division
-        emp[i].da = emp[i].basic_salary * (15.0 / 100);   // Added .0 for correct division
+        printf("Enter the  hra percentage: ");
+        scanf("%f", &hrap);
+        emp[i].hra = emp[i].basic_salary * (hrap / 100.0); // Added .0 for correct division
+        printf("Enter the  da percentage: ");
+        scanf("%f", &dap);
+        emp[i].da = emp[i].basic_salary * (dap / 100.0); // Added .0 for correct division
     }
-    
+
     printf("The details of employees are:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         float grs;
         printf("Name:         %s\n", emp[i].Name);
         printf("Designation:  %s\n", emp[i].Designation);
@@ -39,6 +48,6 @@ int main() {  // Use int instead of void for main
         grs = emp[i].basic_salary + emp[i].da + emp[i].hra;
         printf("Gross Salary: %f\n", grs);
     }
-    
-    return 0;  // Return 0 to indicate successful execution
+
+    return 0; // Return 0 to indicate successful execution
 }
